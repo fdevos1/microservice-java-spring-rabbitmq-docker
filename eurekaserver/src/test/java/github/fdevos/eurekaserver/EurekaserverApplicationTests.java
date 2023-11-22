17c1573fe5f4e62ef363e36f0387cfab7984ad1b
fdevos1 <felipe.devosm@gmail.com>
2023-11-22 16:08:41 -0300

    microservice connected with eureka server

---
+++ eurekaserver/src/test/java/github/fdevos/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package github.fdevos.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-client/src/test/java/github/fdevos/msclient/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package github.fdevos.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
